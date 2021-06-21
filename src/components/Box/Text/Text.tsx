9d351ada026c647550444e9b7683879b15c4f875
Marcos Nunes <marcosrenatovnunes@hotmail.com>
2021-06-21 14:14:02 -0300

    feat(#000): initial setup

---
+++ src/components/Box/Text/Text.tsx
@@ -0,0 +1,35 @@
+import React, { useMemo } from 'react';
+import styled from 'styled-components';
+import { theme } from 'styled-tools';
+import { typography, TypographyProps } from 'styled-system';
+import Truncate, { TruncateProps } from 'react-truncate';
+import { Box, BoxProps } from '../Box';
+
+export type TextPropsStyled = BoxProps & TypographyProps;
+
+export type TextProps = TextPropsStyled & {
+    truncate?: TruncateProps;
+    as?: any;
+    htmlFor?: string;
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>;
+
+export const TextStyled = styled(Box).attrs((props) => ({
+    color: props.color || 'black',
+}))<TextProps>`
+    ${typography}
+
+    ::selection {
+        background-color: ${theme('white')};
+    }
+`;
+
+export const Text: React.FC<TextProps> = ({ children, truncate, ...props }) => {
+    const content = useMemo(() => {
+        if (!!truncate) {
+            return <Truncate {...truncate}>{children}</Truncate>;
+        }
+        return children;
+    }, [truncate, children]);
+
+    return <TextStyled {...props}>{content}</TextStyled>;
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { Box } from 'components/Box';
+
+export default function Home() {
+    return <Box>Hello Radix</Box>;
+}
