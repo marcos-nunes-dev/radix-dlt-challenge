5724e994cc186a5ff7b4452ccd0a16fed126f400
Marcos Nunes <marcosrenatovnunes@hotmail.com>
2021-06-28 13:05:07 -0300

    fix(code): update to prod

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,7 @@ export * from 'axios';
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    // change this to absolute if u are running locally
+    // change to http://localhost:3000/api/
+    baseURL: 'https://radix-dlt-challenge.vercel.app/api/',
 });
